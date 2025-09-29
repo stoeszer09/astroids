f9f4a45a4d3e4720b937f16c7b696b937cae672a
stoeszer09 <stoeszer09@gmail.com>
2025-09-28 21:37:55 -0500

    add clock

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
