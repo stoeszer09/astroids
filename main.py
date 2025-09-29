076a631aa060ad87e4d6b08b5f2d2a038f0dbbe8
stoeszer09 <stoeszer09@gmail.com>
2025-09-28 21:42:36 -0500

    add circle sprites class

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
@@ -15,8 +12,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        dt = clock.tick(60) / 1000
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
