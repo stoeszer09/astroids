c545b8eede7f37e33845858d37a4508384deff1f
stoeszer09 <stoeszer09@gmail.com>
2025-10-01 16:51:34 -0500

    add asteroid/shot collision

--- main.py
+++ main.py
@@ -33,6 +33,11 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 return
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
+                
 
         screen.fill("black")
         for obj in drawable:
